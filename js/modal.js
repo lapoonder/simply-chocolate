33ae59ade5f0beeb9832221837d2ba717fb5215a
Aleksandr Davydenko <ozziaqueen@gmail.com>
2024-08-18 22:03:30 +0200

    Finish_v1

--- js/modal.js
+++ js/modal.js
@@ -16,3 +16,22 @@
     refs.modal.classList.toggle('is-open');
   }
 })();
+
+(() => {
+  const refs2 = {
+    // Додати атрибут data-modal-open на кнопку відкриття
+    openModalBtn: document.querySelector('[mobile-modal-open]'),
+    // Додати атрибут data-modal-close на кнопку закриття
+    closeModalBtn: document.querySelector('[mobile-modal-close]'),
+    // Додати атрибут data-modal на бекдроп модалки
+    modal: document.querySelector('[mobile-menu-window]'),
+  };
+
+  refs2.openModalBtn.addEventListener('click', toggleModal);
+  refs2.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs2.modal.classList.toggle('is-open');
+  }
+})();
